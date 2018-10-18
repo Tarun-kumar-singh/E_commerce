da8c0100f205377b774335c7a544f2226625b6e1
Tarun-kumar-singh <tarunks2512@gmail.com>
2018-10-18 20:58:51 +0530

    passport library(2)

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy();
 
 
-
+// Serialization and Deserialization 
 passport.serializeUser(function(user, done){
 done(null, user._id);	
 });
@@ -17,21 +17,29 @@ passport.deserializeUser(function(id, done){
 
 passport.use('local-login',new LocalStrategy({ 
 usernameField: 'email',
-password: 'password',
+passwordfield: 'password',
 passReqToCallback: true
 	 },function(req,email,password,done){
-	 	User.findOne({ email: email, function(err,user){
+	 	User.findOne({ email: email}, function(err,user){ 
 	 		if (err) { return done(err); }
             if(!user){ return done(null,false,req.flash('loginmessage','No user found')); }
              if (!user.comparePassword(password){
-             	return done(null,false,req.flash('loginmessage','wromg password '));
-             	}); 
-             })});
+             	return done(null,false,req.flash('loginmessage','wrong password ')); 
+             	}
+				return done(null,user);
+		}); 
+             });
 return done(null, user);
-	 	}}});
+	 	}));
 
 
-
+// custom function to validate
+export.isAuthenticated = function(req,res, next){
+	if(req.isAuthenticated()){
+	return next();
+	}
+	res.redirect('/login');
+}
 
 
 
